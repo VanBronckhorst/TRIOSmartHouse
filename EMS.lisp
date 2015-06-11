b96e7ef1996f6e8bb6355371b403cb55f9f15168
VanBronckhorst <filippo.pellolio@mail.polimi.it>
2015-06-11 12:36:26 +0200

    Lisp Added

---
+++ EMS.lisp
@@ -0,0 +1,140 @@
+(asdf:operate 'asdf:load-op 'eezot)
+(use-package :trio-utils)
+
+;constants
+(defconstant GO 1)
+(defconstant WARN 0)
+(defconstant MAY 0)
+(defconstant MUST 1)
+(defconstant MAX_FROM_HEM 2)
+
+(defvar pow-domain (loop for i from 0 to 5 collect i))
+(defvar dev-domain (loop for i from 0 to 2 collect i))
+(define-variable consumption pow-domain)
+(define-variable production pow-domain)
+(define-variable washPower dev-domain)
+(define-variable ovenPower dev-domain)
+(define-variable legPower dev-domain)
+(define-variable solarPower dev-domain)
+(define-variable windmillPower dev-domain)
+
+
+;axioms  
+(defvar consumption-Def
+ (-A- x pow-domain (
+  -E- a1 dev-domain(
+  -E- a2 dev-domain(
+  -E- a3 dev-domain(	
+ 	  	<-> (-P-  consumption x) 
+      (&& (-P- washPower a1)
+      	  (-P-  legPower a2)
+      	  (-P- ovenPower a3)
+      	  (= x (+ (+ a1 a2) a3)))))))))
+
+(defvar production-Def
+ (-A- x pow-domain (
+  -E- a1 dev-domain(
+  -E- a2 dev-domain(	
+ 	  	<-> (-P-  production x) 
+      (&& (-P- solarPower a1)
+      	  (-P- windmillPower a2)
+      	  (= x (+ a1 a2) )))))))
+
+(defvar existance
+	(&&
+		(-E- x pow-domain(-P- consumption x))
+		(-E- x pow-domain(-P- production x))
+		(-E- x dev-domain(-P- washPower x))
+		(-E- x dev-domain(-P- ovenPower x))
+		(-E- x dev-domain(-P- legPower x))
+		(-E- x dev-domain(-P- solarPower x))
+		(-E- x dev-domain(-P- windmillPower x))
+
+	)
+)
+
+(defvar unicity-cons-def
+	( -A- x pow-domain(
+	  -A- x2 pow-domain(-> (&& (-P- consumption x) (-P- consumption x2))
+	  					   (= x x2)
+	  					))
+	)
+	)
+
+(defvar unicity-prod-def
+	( -A- x pow-domain(
+	  -A- x2 pow-domain(-> (&& (-P- production x) (-P- production x2))
+	  					   (= x x2)
+	  					))
+	)
+	)
+
+(defvar unicity-wash-def
+	( -A- x dev-domain(
+	  -A- x2 dev-domain(-> (&& (-P- washPower x) (-P- washPower x2))
+	  					   (= x x2)
+	  					))
+	)
+	)
+
+(defvar unicity-oven-def
+	( -A- x dev-domain(
+	  -A- x2 dev-domain(-> (&& (-P- ovenPower x) (-P- ovenPower x2))
+	  					   (= x x2)
+	  					))
+	)
+	)
+
+(defvar unicity-leg-def
+	( -A- x dev-domain(
+	  -A- x2 dev-domain(-> (&& (-P- legPower x) (-P- legPower x2))
+	  					   (= x x2)
+	  					))
+	)
+	)
+
+(defvar unicity-sol-def
+	( -A- x dev-domain(
+	  -A- x2 dev-domain(-> (&& (-P- solarPower x) (-P- solarPower x2))
+	  					   (= x x2)
+	  					))
+	)
+	)
+
+(defvar unicity-wind-def
+	( -A- x dev-domain(
+	  -A- x2 dev-domain(-> (&& (-P- windmillPower x) (-P- windmillPower x2))
+	  					   (= x x2)
+	  					))
+	)
+	)
+
+
+;the system
+(defvar the-system  
+  (alw (&& 
+          consumption-Def
+          production-Def
+          existance
+          unicity-prod-def
+          unicity-oven-def
+          unicity-wash-def
+          unicity-cons-def
+          unicity-leg-def
+          unicity-sol-def
+          unicity-wind-def
+)))      
+
+;;false assertion
+(defvar false-conjecture
+  (som (&& (washPower-is 5)
+  			(consumption-is 4) ) ))
+
+;Zot call
+(eezot:zot 20
+  (&& 
+    the-system
+    ;(!! utility) ;returns UNSAT, since it cannot find counterexamples
+    (!! false-conjecture) ;returns SAT, since it  finds a counterexample
+  ) 
+)
